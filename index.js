03241a05a89f265884e9be16cbd15ce26ea15b32
mdabdulaziz6236 <md.abdulaziz6236@gmail.com>
2025-12-08 07:34:00 +0600

    In the name of Allah Hafez. init: initial project setup and installed cors express dotenv mongodb

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const cors = require("cors");
+const app = express();
+const port = process.env.PORT || 3000;
+require('dotenv').config()
+
+/* middleware */
+app.use(express.json());
+app.use(cors());
+
+
+app.get("/", (req, res) => {
+  res.send("Alhamdulillah. Server is styling....!");
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
